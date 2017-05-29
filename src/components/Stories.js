2cdbe6974b8157d908041bf3099beaa835d5d371
mariam <mariam.abdussalam@andela.com>
2017-05-29 15:07:23 +0100

    format key

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -24,8 +24,8 @@ export default class Stories extends Component {
           const dataArray = []
           results.data.forEach((element) => {
             const dataObj = {
-              "title": element[0],
-              "Descriptions": element[1],
+              "Title": element[0],
+              "Description": element[1],
               "PublishDate": element[2],
               "AuthorsName": element[3],
               "ImageUrl": element[4],
@@ -45,15 +45,15 @@ export default class Stories extends Component {
           <div className="row">
             <div className="col-sm-8">
               {data.map((tile) => (
-                <div key={tile.title} className="media">
+                <div key={tile.Title} className="media">
                   <div className="media-left">
                     <a href={tile.StoryUrl}>
                       <img className="media-object" src={tile.ImageUrl} alt="..." style={{ "maxWidth": "150px" }} />
                     </a>
                   </div>
                   <div className="media-body">
-                    <h4 className="media-heading">{tile.title}</h4>
-                    <p>{tile.Descriptions}</p>
+                    <h4 className="media-heading">{tile.Title}</h4>
+                    <p>{tile.Description}</p>
                     <p>
                       <a href={tile.StoryUrl}>Read more...</a>
                     </p>
@@ -62,11 +62,11 @@ export default class Stories extends Component {
               ))}
             </div>
 
-          <div className="visible-xs" style={{"height": "50px"}}></div>
-          <div className="col-sm-4">
-            <a className="twitter-timeline" href="https://twitter.com/search?q=%F0%9F%87%B3%F0%9F%87%AC" data-widget-id="869091075746607104">Tweets about 🇳🇬</a>
-            <script>{!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + "://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } }(document, "script", "twitter-wjs")}</script>
-      </div>
+            <div className="visible-xs" style={{ "height": "50px" }}></div>
+            <div className="col-sm-4">
+              <a className="twitter-timeline" href="https://twitter.com/search?q=%F0%9F%87%B3%F0%9F%87%AC" data-widget-id="869091075746607104">Tweets about 🇳🇬</a>
+              <script>{!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + "://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } }(document, "script", "twitter-wjs")}</script>
+            </div>
           </div>
         </div>
       </div>
