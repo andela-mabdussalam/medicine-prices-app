7f4fdf31cb9a88ed8499bc1d93d4df47e96d3c6b
mariam <mariam.abdussalam@andela.com>
2017-05-30 14:17:03 +0100

    [fix]:env var

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,21 +34,21 @@ export default class Footer extends Component {
 
           <div className="post-credits col-sm-6 center-block">
             <p>
-              Produced by <a href="http://punchng.com" target="_blank">PUNCH </a>
-              in partnership with the <a href="https://investigativecenters.org" target="_blank">
+              Produced by <a href="http://punchng.com" rel="noopener noreferrer" target="_blank">PUNCH </a>
+              in partnership with the <a href="https://investigativecenters.org" rel="noopener noreferrer" target="_blank">
                 African Network of Centres for Investigative Reporting (ANCIR) </a>
               with technology support
-              from <a href="https://codefornigeria.org/" target="_blank">Code for Nigeria</a>.
+              from <a href="https://codefornigeria.org/" rel="noopener noreferrer" target="_blank">Code for Nigeria</a>.
                ANCIR's Cost of Life project seeks to shine an investigative light on the
               human cost of profiteering and abuses in the healthcare industry.
             </p>
-            <p>Funded by the <a href="http://www.gatesfoundation.org/" target="_blank">Bill and Melinda Gates Foundation </a>
-              through the <a href="http://www.icfj.org" target="_blank">International Center for Journalists</a>.
+            <p>Funded by the <a href="http://www.gatesfoundation.org/" rel="noopener noreferrer" target="_blank">Bill and Melinda Gates Foundation </a>
+              through the <a href="http://www.icfj.org" rel="noopener noreferrer" target="_blank">International Center for Journalists</a>.
             </p>
           </div>
-          <a href="http://punchng.com" className="sponsors-link" target="_blank"><img className="sponsors" alt="Punch Nigeria" src="/punch-ng-logo-white.png" /></a>
-          <a href="http://www.icfj.org" className="sponsors-link" target="_blank"><img className="sponsors" alt="ICFJ" src="/icfj-white.png" /></a>
-          <a href="http://www.codeforafrica.org" className="sponsors-link" target="_blank"><img className="sponsors" alt="Code for Africa" src="/cfafrica-white.png" /></a>
+          <a href="http://punchng.com" rel="noopener noreferrer" className="sponsors-link" target="_blank"><img className="sponsors" alt="Punch Nigeria" src="/punch-ng-logo-white.png" /></a>
+          <a href="http://www.icfj.org" rel="noopener noreferrer" className="sponsors-link" target="_blank"><img className="sponsors" alt="ICFJ" src="/icfj-white.png" /></a>
+          <a href="http://www.codeforafrica.org" rel="noopener noreferrer" className="sponsors-link" target="_blank"><img className="sponsors" alt="Code for Africa" src="/cfafrica-white.png" /></a>
         </div>
       </footer >
     )
