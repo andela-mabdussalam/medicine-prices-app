daeaa7c2e55a56ee1a91ac8c58fd874e316ed2cd
mariam <mariam.abdussalam@andela.com>
2017-06-06 14:24:20 +0100

    format logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ class NavigationBar extends Component {
       <Navbar fixedTop>
         <Navbar.Header>
           <Navbar.Brand>
-            <NavLink to="/" onClick={this.props.resetCalculator}><p style={{"marginTop": "19px", "fontSize": "22px"}}>CheckYourMedPrices</p></NavLink>
+            <NavLink to="/" onClick={this.props.resetCalculator}><h2 style={{"marginTop": "14px", "color":"rgb(5, 47, 76)"}}>#CheckYourMedPrices</h2></NavLink>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
